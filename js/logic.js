6dd804371c974ea9826a33448b6d90ee26748626
JordanR159 <reed226@purdue.edu>
2017-01-14 15:36:09 -0500

    Size and velocity init

--- js/logic.js
+++ js/logic.js
@@ -25,26 +25,49 @@ function update() {
 function generateAsteroids();
 
 var baseVel = 100;
-var baseSize = 100;
+var baseRad = 50;
 var sun = Box2D.Collision.Shapes.b2CircleShape;
 var planets = [];
 
 function initWorld()
 {
-	sun.m_position.Set(screen.width/2, screen.height/2);
+	sun.m_position.Set(screenWidth/2, screenHeight/2);
+	sun.m_radius = baseRad*5;
 	
 	for(var i = 1; i < 11; i++)
 	{
 		var planet = Box2D.Collision.Shapes.b2CircleShape;
-		planet.m_position.Set(screen.width*i/11, screen.height/2);
+		planet.m_position.Set(screenWidth/2, screenHeight*i/11);
 		planets.push(planet);
 		if(i == 4)
 		{
 			i+= 2;
 		}
 	}
-	planets[1].
+	planets[4].m_radius = baseRad;
+	planets[7].m_radius = baseRad;
+	planets[3].m_radius = baseRad*1.5;
+	planets[8].m_radius = baseRad*1.5;
+	planets[2].m_radius = baseRad*2.25;
+	planets[9].m_radius = baseRad*2.25;
+	planets[1].m_radius = baseRad*3;
+	planets[10].m_radius = baseRad*3;
+	
+	planets[4].m_velocity.Set(baseVel, 0);
+	planets[7].m_velocity.Set(baseVel, 0);
+	planets[3].m_velocity.Set(-baseVel/1.4, 0);
+	planets[8].m_velocity.Set(-baseVel/1.4, 0);
+	planets[2].m_velocity.Set(baseVel/1.7, 0);
+	planets[9].m_velocity.Set(baseVel/1.7, 0);
+	planets[1].m_velocity.Set(baseVel/2, 0);
+	planets[10].m_velocity.Set(baseVel/2, 0);
+	
 	ready = true;
+}
+
+function movePlanets()
+{
+	for()
 }
 
 
